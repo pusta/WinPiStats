e2701037b26a20280ed0c8204eb3fda64d868364
Chris Kapusta <ckapusta@outlook.com>
2020-06-21 20:14:22 -0500

    Cleanup settings storage and move to class

---
+++ src/WinPiStats/Controls/Settings/PiHoleSettings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace WinPiStats.Controls.Settings
+{
+    class PiHoleSettings
+    {
+        public string piHoleName { get; set; }
+        public string piHoleAddress { get; set; }
+        public string piHoleAuthKey { get; set; }
+
+        ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+
+
+        public PiHoleSettings(string piholeServerNumber)
+        {
+
+            piHoleName = localSettings.Values["PiHoleServerName" + '-' + piholeServerNumber].ToString();
+            piHoleAddress = localSettings.Values["PiHoleAddress" + '-' + piholeServerNumber].ToString();
+            piHoleAuthKey = localSettings.Values["PiHoleAuthKey" + '-' + piholeServerNumber].ToString();
+
+
+        }
+
+
+
+    }
+}
--- src/WinPiStats/Views/Content/ContentPage.xaml.cs
+++ src/WinPiStats/Views/Content/ContentPage.xaml.cs
@@ -17,6 +17,7 @@ using Windows.Storage;
 using System.Drawing;
 using Windows.Data.Text;
 using System.Collections.ObjectModel;
+using WinPiStats.Controls.Settings;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -28,11 +29,8 @@ namespace WinPiStats.Views.Content
     public sealed partial class ContentPage : Page
     {
 
-        ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
-        private string piholeAuthkey;
-        private string piholeAddress;
-        private string piholeName;
-        private string piholeServerNumber;   
+        private string piholeServerNumber;
+        private PiHoleSettings piHoleSettings;
         
         
         
@@ -43,7 +41,7 @@ namespace WinPiStats.Views.Content
             InitializeComponent();
            
 
-            //ReadSettings();
+            
             
 
            
@@ -54,17 +52,17 @@ namespace WinPiStats.Views.Content
         private void PiholeStateCheck()
         {
 
-            var piholeapi = new PiHoleAPI(piholeAddress, piholeAuthkey);
+            var piholeapi = new PiHoleAPI(piHoleSettings.piHoleAddress, piHoleSettings.piHoleAuthKey);
             if (piholeapi.Is_Pihole_Enabled())
             {
-                //isEnabledTextBlock.Text = "Enabled";
+                
                 GreenDot.Visibility = Visibility.Visible;
                 RedDot.Visibility = Visibility.Collapsed;
                 toggleStateButton.Content = "Disable";
             }
             else
             {
-               // isEnabledTextBlock.Text = "Disabled";
+               
                 GreenDot.Visibility = Visibility.Collapsed;
                 RedDot.Visibility = Visibility.Visible;
                 toggleStateButton.Content = "Enable";
@@ -75,7 +73,7 @@ namespace WinPiStats.Views.Content
 
         private void toggleStateButton_Click(object sender, RoutedEventArgs e)
         {
-            var piholeapi = new PiHoleAPI(piholeAddress, piholeAuthkey);
+            var piholeapi = new PiHoleAPI(piHoleSettings.piHoleAddress, piHoleSettings.piHoleAuthKey);
 
             if (piholeapi.Is_Pihole_Enabled())
                 piholeapi.Pihole_Change_State("disable");
@@ -90,16 +88,15 @@ namespace WinPiStats.Views.Content
         private void UpdatePiInfo()
         {
 
-            var piholeapi = new PiHoleAPI(piholeAddress, piholeAuthkey);
+            var piholeapi = new PiHoleAPI(piHoleSettings.piHoleAddress, piHoleSettings.piHoleAuthKey);
 
             blockedTextBlock.Text = piholeapi.Query_Pihole_Domains();
             queriesTodayTextBlock.Text = piholeapi.Total_Queries_Today();
-            piholeNameTextBlock.Text = piholeName;
+            piholeNameTextBlock.Text = piHoleSettings.piHoleName;
             AdsBlockedTextBlock.Text = piholeapi.Ads_Blocked();
             PercentAdsBlockedTextBlock.Text = piholeapi.Ads_Percent_Blocked() + "%";
             TopItemsTextBlock.Text = piholeapi.topItems();
             LastBlockTextBlock.Text = piholeapi.Most_Recent_Blocked();
-            //LastBlockTextBlock.Text = piholeServerNumber;
             TopClientsTextBlock.Text = piholeapi.topClients();
 
            
@@ -112,33 +109,15 @@ namespace WinPiStats.Views.Content
             piholeServerNumber = e.Parameter as string;
 
 
-
-
-
         }
 
-        private void ReadSettings()
-        {
-            var numPiholes = localSettings.Values["NumberofServers"];
-
-            /*piholeName = localSettings.Values["PiHoleServerName" + '-' + localSettings.Values["NumberofServers"].ToString()].ToString();
-            piholeAddress = localSettings.Values["PiHoleAddress" + '-' + localSettings.Values["NumberofServers"].ToString()].ToString();
-            piholeAuthkey = localSettings.Values["PiHoleAuthKey" + '-' + localSettings.Values["NumberofServers"].ToString()].ToString();*/
-
-            piholeName = localSettings.Values["PiHoleServerName" + '-' + piholeServerNumber].ToString();
-            piholeAddress = localSettings.Values["PiHoleAddress" + '-' + piholeServerNumber].ToString();
-            piholeAuthkey = localSettings.Values["PiHoleAuthKey" + '-' + piholeServerNumber].ToString();
-
-
-
-
-
-        }
+        
 
         private void Page_Loading(FrameworkElement sender, object args)
         {
 
-            ReadSettings();
+           
+            piHoleSettings = new PiHoleSettings(piholeServerNumber);
             UpdatePiInfo();
 
             PiholeStateCheck();
