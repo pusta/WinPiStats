ed32cf28139a14bee01fbb81a45fd68759322083
Chris Kapusta <ckapusta@outlook.com>
2020-06-22 11:32:52 -0500

    Renaming Application

--- src/WinPiStats/Properties/AssemblyInfo.cs
+++ src/WinPiStats/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WinPiStats")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Centaurus")]
+[assembly: AssemblyDescription("A Windows PiHole Monitoring Application")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WinPiStats")]
+[assembly: AssemblyCompany("214 Technologies")]
+[assembly: AssemblyProduct("Centaurus")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -27,3 +28,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
